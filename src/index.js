d03fb2606f67f8dcd082c7030f213b738f8f6963
aldrincantero <aldrin.cantero@gmail.com>
2021-07-26 17:44:56 +0800

    Restore to previous code

--- src/index.js
+++ src/index.js
@@ -3,27 +3,14 @@ const EventBusFactory = () => {
 
     const fire = (event, payload) => {
         // publish an event listener
-        if (!listeners[event]) return [];
-
-        listeners[event] = listeners[event].map(callback => callback(payload));
-
-        return listeners[event];
     };
 
     const listen = (event, listener) => {
         // create an event listener
-        if (!listeners[event]) {
-            listeners[event] = [];
-        }
-
-        listeners[event].push(listener);
     };
 
     const unsubscribe = (event, removeListener) => {
         // unsubscribe a listener from event
-        if (listeners[event]) {
-            listeners[event] = listeners[event].filter(callback => callback !== removeListener);
-        }
     };
 
     return {
